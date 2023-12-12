6227dd607b6e5fcaf3e7dd2e4cf9d82180d52ec7
IvanStark <104026643+AzrealStillCute@users.noreply.github.com>
2023-12-12 21:13:38 +0630

    add viewport restriction

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,48 @@
-import { Suspense, lazy } from "react";
+import { Suspense, lazy, useEffect, useState } from "react";
 import ContextProvider from "./components/ContextProvider";
 import Loading from "./components/Loading";
-import { ArrowUpRight } from "@phosphor-icons/react";
+import { ArrowUpRight, WarningCircle } from "@phosphor-icons/react";
 
 const SideBar = lazy(() => import("./layout/SideBar"));
 const NavBar = lazy(() => import("./layout/NavBar"));
 const Canvas = lazy(() => import("./layout/Canvas"));
 
 function App() {
+  const [view, setView] = useState(window.innerWidth);
+
+  useEffect(() => {
+    window.addEventListener("resize", setWidthToView);
+  });
+
+  function setWidthToView() {
+    setView(window.innerWidth);
+  }
+
   return (
     <main className=" w-screen h-screen bg-neutral-200 dark:bg-neutral-900 font-primary">
-      <Suspense fallback={<Loading />}>
-        <ContextProvider>
-          <NavBar />
-          <div className=" flex h-body">
-            <Canvas />
-            <SideBar />
-          </div>
-        </ContextProvider>
-      </Suspense>
-      <a href="https://github.com/CraziiIvan/code-frame" target="_blank" className="absolute bottom-3 left-3 text-sm text-neutral-500 dark:text-neutral-600 cursor-pointer">
-        Github Repo <ArrowUpRight className="inline" />
-      </a>
+      {view < 1200 ? (
+        <div className=" h-full flex flex-col gap-y-5 items-center justify-center">
+          <WarningCircle className="text-neutral-300 dark:text-neutral-800" size={80}/>
+          <h5 className=" px-5 text-sm text-center text-neutral-400 dark:text-neutral-600">This site is optimized for a minimum width of 1200 pixels or higher.<br/> Please ensure that your screen size meets this requirement for an optimal browsing experience.</h5>
+        </div>
+      ) : (
+        <Suspense fallback={<Loading />}>
+          <ContextProvider>
+            <NavBar />
+            <div className=" flex h-body">
+              <Canvas />
+              <SideBar />
+            </div>
+          </ContextProvider>
+          <a
+            href="https://github.com/CraziiIvan/code-frame"
+            target="_blank"
+            className="absolute bottom-3 left-3 text-sm text-neutral-500 dark:text-neutral-600 cursor-pointer"
+          >
+            Github Repo <ArrowUpRight className="inline" />
+          </a>
+        </Suspense>
+      )}
     </main>
   );
 }
